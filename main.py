660d1d2a256e557abfd4ee1b64be9db18881c29c
JelleWierenga <116724275+JelleWierenga@users.noreply.github.com>
2023-05-16 13:40:12 +0200

    random words

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-Hidden_word = 'test'
-guese = input("You're guese: ")
+import random
+score = 0
+word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "imbu", "jackfruit", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine"]
+Hidden_word = random.sample(word_list, 1)[0]
 
-if guese == Hidden_word:
-    print(f"congrats you guesed the word the word was {Hidden_word}")
-else:
-    print(f"Try again")
+while score < 10:
+    guese = input("You're guese: ")
+    if guese == Hidden_word:
+        score = score + 1
+        print(f"congrats you guesed the word! The hidden word was {Hidden_word} and you're score is {score}")
+        break
+    else:
+        print(f"Try again")
+        score = score + 1
+print(f"You're out of gueses the hidden word was {Hidden_word}")
