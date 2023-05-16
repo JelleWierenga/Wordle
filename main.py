1bb9dd21508a06de90ce25ca64ca7a7ce3f7fd85
JelleWierenga <116724275+JelleWierenga@users.noreply.github.com>
2023-05-16 13:26:36 +0200

    first prototype

---
+++ main.py
@@ -0,0 +1,7 @@
+Hidden_word = 'test'
+guese = input("You're guese: ")
+
+if guese == Hidden_word:
+    print(f"congrats you guesed the word the word was {Hidden_word}")
+else:
+    print(f"Try again")
